28a48ce493d6f1335cc7faa061082b75736bbcf9
hinacarlo <hinacarlo1993@gmail.com>
2023-02-22 00:01:55 +0800

    Add the structure of the class Kanban in kanban.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("hello world");
+import Kanban from "./kanban";
---
+++ kanban.js
@@ -0,0 +1,15 @@
+export default class Kanban {
+  static getTasks(columnId) {}
+
+  static createTask(columnId, content) {}
+
+  static updateTask(taskId, updatedContent) {}
+
+  static deleteTask(taskId) {}
+
+  static getAllTasks() {}
+}
+
+function read() {}
+
+function save() {}
